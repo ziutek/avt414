bb7f21ef361da641d79c32d30cb047412b8a4d63
Michał Derkacz <michal@Lnet.pl>
2013-06-07 21:39:51 +0200

    Improved performance. New Writer method.

--- avt414.go
+++ avt414.go
@@ -3,6 +3,7 @@ package avt414
 import (
 	"fmt"
 	"github.com/ziutek/serial"
+	"io"
 )
 
 const esc = 0x1b
@@ -10,13 +11,17 @@ const esc = 0x1b
 var hex = [16]byte{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 	'A', 'B', 'C', 'D', 'E', 'F'}
 
-type Avt414 struct {
-	s *serial.Serial
+type AVT414 struct {
+	s     *serial.Serial
+	lastR bool // last operation was read from binary port
+	lastW byte // last written port
+	lastA byte // last readed ADC channel
+	cmd   [5]byte
 }
 
-// Open connection to the device.
+// Open opens an AVT414 device.
 // name - path to the device file (eg: /dev/ttyUSB0)
-func Open(name string) (*Avt414, error) {
+func Open(name string) (*AVT414, error) {
 	s, err := serial.Open(name)
 	if err != nil {
 		return nil, err
@@ -24,10 +29,14 @@ func Open(name string) (*Avt414, error) {
 	if err = s.SetSpeed(57600); err != nil {
 		return nil, err
 	}
-	return &Avt414{s}, nil
+	a := AVT414{s: s}
+	a.lastA = 0xff
+	a.cmd[0] = esc
+	return &a, nil
 }
 
-func (a *Avt414) Close() error {
+// Close closes a device
+func (a *AVT414) Close() error {
 	err := a.s.Close()
 	a.s = nil
 	return err
@@ -40,10 +49,10 @@ func checkPort(port byte) error {
 	return nil
 }
 
-// Setups lines in specified port for input/output
-//  port: 'B', 'C' or 'D'
+// Setup setups lines in specified port for input/output
+//  port:  'B', 'C' or 'D'
 //  iomask: 0 - output, 1 - input
-func (a *Avt414) Setup(port, iomask byte) error {
+func (a *AVT414) Setup(port, iomask byte) error {
 	err := checkPort(port)
 	if err != nil {
 		return err
@@ -53,61 +62,142 @@ func (a *Avt414) Setup(port, iomask byte) error {
 		iomask &^= 0x40
 		iomask |= 0x80
 	}
-	cmd := []byte{
-		esc, 'S', port,
-		hex[(iomask>>4)&0xf], hex[iomask&0xf],
-	}
-	_, err = a.s.Write(cmd)
+	// Reset internal state
+	a.lastR = false
+	a.lastW = 0
+	a.lastA = 0xff
+	// Write a setup command
+	a.cmd[1] = 'S'
+	a.cmd[2] = port
+	a.cmd[3] = hex[(iomask>>4)&0xf]
+	a.cmd[4] = hex[iomask&0xf]
+	_, err = a.s.Write(a.cmd[:5])
 	return err
 }
 
-// Write to port
-func (a *Avt414) Write(port, val byte) error {
+// Write writes one byte to the port. It uses two byte abbreviated (fast)
+// command for subsequent writes to the same port. See Writer for write byte
+// slice to the same port.
+func (a *AVT414) Write(port, val byte) error {
 	err := checkPort(port)
 	if err != nil {
 		return err
 	}
-	cmd := []byte{
-		esc, 'W', port,
-		hex[(val>>4)&0xf], hex[val&0xf],
+	// Common parameters for two kinds of wirite command
+	a.cmd[3] = hex[(val>>4)&0xf]
+	a.cmd[4] = hex[val&0xf]
+	if a.lastW == port {
+		// Write an abbreviated command
+		_, err = a.s.Write(a.cmd[3:5])
+	} else {
+		a.lastR = false
+		a.lastW = port
+		a.lastA = 0xff
+		// Write a full command
+		a.cmd[1] = 'W'
+		a.cmd[2] = port
+		_, err = a.s.Write(a.cmd[:5])
 	}
-	_, err = a.s.Write(cmd)
 	return err
 }
 
-// Reads from port
-func (a *Avt414) Read(port byte) (byte, error) {
+// Read reads from port. It uses one byte abbreviated command for subsequent
+// reads.
+func (a *AVT414) Read(port byte) (byte, error) {
 	err := checkPort(port)
 	if err != nil {
 		return 0, err
 	}
-	cmd := []byte{esc, 'R', port, 'B'}
-	if _, err = a.s.Write(cmd); err != nil {
+	// Common parameter for two kinds of read command
+	a.cmd[2] = port
+	if a.lastR {
+		// Write an abbreviated command
+		_, err = a.s.Write(a.cmd[2:3])
+	} else {
+		a.lastR = true
+		a.lastW = 0
+		a.lastA = 0xff
+		// Write a full command
+		a.cmd[1] = 'R'
+		a.cmd[3] = 'B'
+		_, err = a.s.Write(a.cmd[:4])
+	}
+	if err != nil {
 		return 0, err
 	}
 	return a.s.ReadByte()
 }
 
-// Reads 10-bit value from specified ADC line
-//  0 <= line <= 7
-func (a *Avt414) ADC(line int) (uint16, error) {
-	if line < 0 || line > 7 {
+// Reads 10-bit value from specified ADC line (0 <= line <= 7). It uses one
+// byte abbreviated command for subsequent reads.
+func (a *AVT414) ADC(line byte) (uint16, error) {
+	if line > 7 {
 		return 0, fmt.Errorf("Bad AVT414 ADC line number: %d", line)
 	}
-	cmd := []byte{esc, 'A', 'B', hex[line]}
-	_, err := a.s.Write(cmd)
+	var err error
+	// Common parameter for two kinds of ADC command
+	a.cmd[3] = hex[line]
+	if a.lastA == line {
+		// Write an abbreviated command
+		_, err = a.s.Write(a.cmd[3:4])
+	} else {
+		a.lastR = false
+		a.lastW = 0
+		a.lastA = line
+		// Write a full command
+		a.cmd[1] = 'A'
+		a.cmd[2] = 'B'
+		_, err = a.s.Write(a.cmd[:4])
+	}
 	if err != nil {
 		return 0, err
 	}
-	n, err := a.s.Read(cmd[:2])
+	n, err := a.s.Read(a.cmd[3:5])
 	if err != nil {
 		return 0, err
 	}
 	if n == 1 {
-		_, err = a.s.Read(cmd[1:2])
+		_, err = a.s.Read(a.cmd[4:5])
 		if err != nil {
 			return 0, err
 		}
 	}
-	return (uint16(cmd[0]) << 8) | uint16(cmd[1]), nil
+	return (uint16(a.cmd[3]) << 8) | uint16(a.cmd[4]), nil
+}
+
+type writer struct {
+	port byte
+	a    *AVT414
+}
+
+func (w writer) Write(buf []byte) (n int, err error) {
+	if len(buf) == 0 {
+		return
+	}
+	err = w.a.Write(w.port, buf[0])
+	if err != nil {
+		return
+	}
+	n++
+	var (
+		b   byte
+		cmd = w.a.cmd[3:5]
+		s   = w.a.s
+	)
+	for _, b = range buf[1:] {
+		cmd[0] = hex[(b>>4)&0xf]
+		cmd[1] = hex[b&0xf]
+		_, err = s.Write(cmd)
+		if err != nil {
+			return
+		}
+		n++
+	}
+	return
+}
+
+// Writer returns io.Writer that can be used for fast write byte slices to the
+// specified port.
+func (a *AVT414) Writer(port byte) io.Writer {
+	return writer{port, a}
 }
--- examples/test.go
+++ examples/test.go
@@ -6,38 +6,57 @@ import (
 	"time"
 )
 
-func main() {
-	a, err := avt414.Open("/dev/ttyUSB0")
+func checkErr(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func main() {
+	a, err := avt414.Open("/dev/ttyUSB0")
+	checkErr(err)
 
 	port := byte('D')
+	checkErr(a.Setup(port, 0))
 
-	if err = a.Write(port, 0); err != nil {
-		log.Fatal(err)
+	buf := []byte("blaaaaa")
+	n, err := a.Writer(port).Write(buf)
+	checkErr(err)
+	if n != len(buf) {
+		panic("bad n")
 	}
-	if err = a.Setup(port, 0); err != nil {
-		log.Fatal(err)
-	}
+
+	const loop = 20
 	var i byte
 	for {
+		start := time.Now()
 		if i == 0 {
 			i = 0x80
 		}
-		if err = a.Write(port, i); err != nil {
-			log.Fatal(err)
+		for n := 0; n < loop; n++ {
+			if err = a.Write(port, i); err != nil {
+				log.Fatal(err)
+			}
 		}
-		v, err := a.Read('B')
-		if err != nil {
-			log.Fatal(err)
+		var b byte
+		for n := 0; n < loop; n++ {
+			b, err = a.Read('B')
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
-		a, err := a.ADC(0)
-		if err != nil {
-			log.Fatal(err)
+		var c uint16
+		for n := 0; n < loop; n++ {
+			c, err = a.ADC(0)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
-		log.Printf("%x, %.3f", v, 2.5 * float32(a) / 1023)
-		time.Sleep(100 * time.Millisecond)
+		dt := time.Now().Sub(start)
+		log.Printf(
+			"%x, %.3f  %d cmd/s",
+			b, 2.5*float32(c)/1023, 3*loop*time.Second/dt,
+		)
 		i >>= 1
 	}
 	a.Close()
